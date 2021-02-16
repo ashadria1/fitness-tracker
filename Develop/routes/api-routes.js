36f33069c66033a3d176c685325b117b2596ad4c
ashadria <ashadria@gmail.com>
2021-02-16 07:09:14 -0500

    adding range route

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -6,6 +6,7 @@ module.exports = function (app) {
   app.get("/api/workouts", function (req, res) {
     db.Workout.find({})
     .then(dbWorkout => {
+      console.log(dbWorkout);
       res.json(dbWorkout);
     })
     .catch(err => {
@@ -14,7 +15,7 @@ module.exports = function (app) {
     
   });
   
-  //Create Workout session
+  //Create Workout Session
   app.post("/api/workouts", function (req, res) {
     db.Workout.create({})
     .then(dbWorkout => {
@@ -26,7 +27,7 @@ module.exports = function (app) {
     });
   });
 
-    //Add Exercise to a workout session
+    //Add Exercise to a Workout Session
     app.put("/api/workouts/:workoutId", function (req, res) {
       console.log(req.params.workoutId);
       db.Workout.create({})
@@ -38,7 +39,15 @@ module.exports = function (app) {
         res.json(err);
       });
     });
-
+/* 
+    async getWorkoutsInRange() {
+      const res = await fetch(`/api/workouts/range`);
+      const json = await res.json();
+  
+      return json;
+    },
+     */
+/* 
     // GET route to return the last 7 workouts
     Router.get("/api/workouts/range", (req,res) => {
       Workout.find({})
@@ -50,5 +59,40 @@ module.exports = function (app) {
         res.json(err);
       });
     });
+
+    // workouts in range
+// sort by day and limit to one week (7 days)
+router.get("/api/workouts/range", (req, res) => {
+  let oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 8);
+  // let oneWeekAgoUnix = Date.parse(oneWeekAgo);
+  db.Workout.find({})
+    .sort({ day: 1 })
+    .where("day")
+    .gte(oneWeekAgo)
+    // .where("day").gte(oneWeekAgoUnix)
+    // .limit(7)
+
+    .then((weekWorkouts) => {
+      res.json(weekWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// GET route to find last 7 workouts
+Router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+      .limit(7)
+      .then((workouts) => {
+          res.json(workouts);
+      })
+      .catch((err) => {
+          res.json(err);
+      });
+});
+ */
+
 };
 
