436ccfb9779d26fec18bddf10777fe9d2cae63c7
ashadria <ashadria@gmail.com>
2021-02-21 17:14:33 -0500

    moving routes folder

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require("./models");
 const PORT = process.env.PORT || 8080;
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.MONGODB_URI || 'mongodb://localhost/workouts',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
